b5e6aa7c717cb39c13bb0ac44de1643980490ff0
RabronixCode <letscodeit10@gmail.com>
2025-01-03 13:31:38 +0100

    Finished with top articles - 9 of them on a page

--- crypto_news.py
+++ crypto_news.py
@@ -3,7 +3,12 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
 import time
+import threading
+
 
 url = 'https://crypto.news/news/'
 
@@ -14,15 +19,35 @@ driver = webdriver.Chrome(service=service)
 driver.get(url)
 
 wait = WebDriverWait(driver, 20)
-# Waiting for all global element hookups
-driver.implicitly_wait(5)
-
-# Clicking notification CANCEL
-cancel_button = driver.find_element(By.ID, "onesignal-slidedown-cancel-button")
-cancel_button.click()
 
 # Clicking cookie ACCEPT
 cookie_button = driver.find_element(By.ID, "cookie-consent-button")
 cookie_button.click()
 
-time.sleep(1000)
+i = 0
+popup = True
+while True:
+    top_articles = driver.find_elements(By.CLASS_NAME, "post-loop__link")
+    if i == len(top_articles):
+        break
+    driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", top_articles[i])
+    time.sleep(2)
+    if popup:
+        popups = driver.find_element(By.ID, "onesignal-slidedown-cancel-button")
+        popups.click()
+        popup = False
+
+    
+    title = top_articles[i].text
+    top_articles[i].click()
+    text_elements = driver.find_elements(By.XPATH, "/html/body//article//div[@class='post-detail__content blocks']/p")
+    for t in text_elements:
+        with open(f"{title}.txt", "a", encoding="utf-8") as file:
+            file.write(f"{t.text} \n")
+    driver.back()
+    i+=1
+    
+
+
+
+time.sleep(100)
