81b5e6771f8199ac4c0193dbb44d3dfd40f23459
RabronixCode <letscodeit10@gmail.com>
2025-01-03 10:16:23 +0100

    Clicking on Cancel for notif and Accept for cookies

--- crypto_news.py
+++ crypto_news.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 url = 'https://crypto.news/news/'
 
@@ -11,3 +14,15 @@ driver = webdriver.Chrome(service=service)
 driver.get(url)
 
 wait = WebDriverWait(driver, 20)
+# Waiting for all global element hookups
+driver.implicitly_wait(5)
+
+# Clicking notification CANCEL
+cancel_button = driver.find_element(By.ID, "onesignal-slidedown-cancel-button")
+cancel_button.click()
+
+# Clicking cookie ACCEPT
+cookie_button = driver.find_element(By.ID, "cookie-consent-button")
+cookie_button.click()
+
+time.sleep(1000)
